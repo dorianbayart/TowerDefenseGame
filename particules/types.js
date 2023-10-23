23c8879a6d08a4993acc96133ebd1f36276ecfc1
Dorian Bayart <dorianbayart@users.noreply.github.com>
2023-10-23 23:56:23 +0200

    Update types.js

--- particules/types.js
+++ particules/types.js
@@ -4,18 +4,32 @@ var MISSILE_TYPES = {
   NORMAL: {
     mesh: undefined,
     speed: 8
+  },
+  ROCKET: {
+    mesh: undefined,
+    speed: 3
   }
 };
 
 var PARTICULE_TYPES = {
   NORMAL: {
-    lifespan: 1,
+    lifespan: 10,
     explodeEffet: 1,
     number: {
-      min: 3,
-      max: 8
+      min: 15,
+      max: 40
     },
     size: 0.1,
     mesh: undefined,
-  }
+  },
+  ROCKET: {
+    lifespan: 1,
+    explodeEffet: 2.5,
+    number: {
+      min: 8,
+      max: 15
+    },
+    size: 0.12,
+    mesh: undefined,
+  },
 }
