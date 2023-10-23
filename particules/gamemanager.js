755ebbf28571606ca2c50ec544036599c42dbdb2
Dorian Bayart <dorianbayart@users.noreply.github.com>
2023-10-24 01:17:40 +0200

    Pop a mob if there is no one on the map

--- particules/gamemanager.js
+++ particules/gamemanager.js
@@ -22,11 +22,15 @@ class Game {
 class GameManager {
     constructor() {
         this.game = new Game(10, 10);
+        this.timer = clock.elapsedTime;
 
         setInterval(() => {
-            var elapsed = clock.elapsedTime;
-            mobsManager.createMob(mobMesh, scene, elapsed);
-          }, 5000
+            const elapsed = clock.elapsedTime; // in seconds
+            if(elapsed - this.timer > 3 || mobsManager.mobArray.length === 0) {
+              mobsManager.createMob(mobMesh, scene);
+              this.timer = elapsed;
+            }
+          }, 250
         );
     }
 
