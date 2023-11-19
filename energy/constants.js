ada29fad42fe8a8fbd6ae57ca534269dad11205a
Dorian Bayart <dorianbayart@users.noreply.github.com>
2023-11-19 23:46:10 +0100

    Add more colors

--- energy/constants.js
+++ energy/constants.js
@@ -4,17 +4,35 @@ export const mazeSize = 15;
 export const objectsMargin = 1;
 
 export const COLOR = {
-  BLACK: '#181818',
-  BLUE: 'cornflowerblue',
-  BROWN: 'sienna',
-  DARKGRAY: 'dimgray',
-  GREEN: 'mediumseagreen',
-  GRAY: 'gray',
-  INDIGO: 'mediumslateblue',
+  BLACK: '#17202a',
+  BLUE: '#2874a6',
+  BROWN: '#ba4a00', //#A0522D = sienna
+  DARKGRAY: '#626567',
+  GREEN: '#1e8449',
+  GRAY: '#909497',
+  INDIGO: '#884ea0',
   LAVENDER: 'lavender',
-  LIGHTGRAY: 'silver',
-  RED: 'crimson',
-  SALMON: 'lightsalmon'
+  LIGHTBLUE: '#85c1e9',
+  LIGHTBROWN: '#edbb99',
+  LIGHTGRAY: '#d7dbdd',
+  LIGHTGREEN: '#7dcea0',
+  LIGHTINDIGO: '#d7bde2',
+  LIGHTORANGE: '#f8c471',
+  LIGHTRED: '#d98880',
+  LIGHTSTEEL: '#85929e',
+  LIGHTERBLUE: '#3498db',
+  LIGHTERBROWN: '#dc7633',
+  LIGHTERGRAY: '#bdc3c7',
+  LIGHTERGREEN: '#27ae60',
+  LIGHTERINDIGO: '#af7ac5',
+  LIGHTERORANGE: '#f39c12',
+  LIGHTERRED: '#c0392b',
+  LIGHTERSTEEL: '#34495e',
+  ORANGE: '#b9770e',
+  RED: '#922b21',
+  SALMON: 'lightsalmon',
+  STEEL: '#283747',
+  WHITE: '#fdfefe'
 }
 
 export const THREE_COLOR = {
